6a98b169a6df855ee6576a9bde3243f9397bdf62
frank <zhumingxu666@outlook.com>
2020-05-07 09:08:46 +0800

    提交代码

--- src/main/java/com/frank/api/gateway/FrankApiApplication.java
+++ src/main/java/com/frank/api/gateway/FrankApiApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+/**
+ * @author frank
+ */
 @SpringBootApplication
 @EnableWebFlux
 public class FrankApiApplication {
--- src/main/java/com/frank/api/gateway/filter/CodecFilter.java
+++ src/main/java/com/frank/api/gateway/filter/CodecFilter.java
@@ -3,7 +3,6 @@ package com.frank.api.gateway.filter;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
